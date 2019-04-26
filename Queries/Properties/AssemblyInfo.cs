f09cc7b454b4fdc731f559c53492768109fd155e
Jassar Al_Akhars <jalakhras@blessedtreeit.com>
2019-04-27 01:20:07 +0300

    LINQ Queries [classification of standard linq operators , Immediate,Deferred,Streaming,and Non-Streaming]

--- Queries/Properties/AssemblyInfo.cs
+++ Queries/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Introduction")]
+[assembly: AssemblyTitle("Queries")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("HP Inc.")]
-[assembly: AssemblyProduct("Introduction")]
+[assembly: AssemblyProduct("Queries")]
 [assembly: AssemblyCopyright("Copyright © HP Inc. 2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("fee6a9d1-a639-4057-b865-d14a7658a811")]
+[assembly: Guid("c8694d82-e067-4bf8-9752-3b99ad231fc9")]
 
 // Version information for an assembly consists of the following four values:
 //
