504263120516090ea8dd6c102883daa67f6e319b
Jassar Al_Akhars <jalakhras@blessedtreeit.com>
2019-04-27 02:23:03 +0300

    Finding the Most Fuel Efficient Car

--- Cars/Program.cs
+++ Cars/Program.cs
@@ -11,11 +11,13 @@ namespace Cars
     {
         static void Main(string[] args)
         {
-           
+            //Note Combined=> Fuel efficiency
             var cars = ProcessFile("fuel.csv");
-            foreach (var car in cars)
+            var query = cars.OrderByDescending(c => c.Combined)
+                .ThenBy(c=>c.Name);
+            foreach (var car in query.Take(10))
             {
-                Console.WriteLine(car.Name);
+                Console.WriteLine($"{car.Name} :{car.Combined}");
             }
         }
 
