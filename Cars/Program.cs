e9d2043aca8e94d7f3ee6238ac1555c4ba38c6b8
Jassar Al_Akhars <jalakhras@blessedtreeit.com>
2019-04-27 18:00:43 +0300

    Aggregating Data

--- Cars/Program.cs
+++ Cars/Program.cs
@@ -60,7 +60,7 @@ namespace Cars
             //}
             #endregion
 
-            #region  #region Grouping Data with Query Syntax 
+            #region Grouping Data with Query Syntax 
             //var query =
             //    from car in cars
             //    group car by car.Manufacturer.ToUpper()
@@ -81,7 +81,6 @@ namespace Cars
 
             //}
             #endregion
-
             #region Grouping Data with Method Syntax 
             //var query2 = cars.GroupBy(c => c.Manufacturer.ToUpper())
             //    .OrderBy(g => g.Key);
@@ -101,58 +100,116 @@ namespace Cars
             #endregion
 
 
-            #region  #region Using a GroupJoin for Hierarchical Data with Query Syntax 
+            #region Using a GroupJoin for Hierarchical Data with Query Syntax 
+            //var query =
+            //    from manufacturer in manufacturers
+            //    join car in cars
+            //    on manufacturer.Name equals car.Manufacturer
+            //    into carGroup
+            //    orderby manufacturer.Name
+            //    select new
+            //    {
+            //        manufacturer = manufacturer,
+            //        cars = carGroup
+            //    };
+
+
+
+            //Console.WriteLine();
+            //Console.WriteLine("***** Grouping Data with Query Syntax  *****");
+            //Console.WriteLine();
+            //foreach (var group in query)
+            //{
+            //    Console.WriteLine($"{group.manufacturer.Name} : {group.manufacturer.Headquarters}");
+            //    foreach (var car in group.cars.OrderByDescending(c => c.Combined).Take(2))
+            //    {
+            //        Console.WriteLine($"\t{car.Name} : {car.Combined}");
+            //    }
+
+            //}
+            #endregion
+            #region Using a GroupJoin for Hierarchical Data with Method Syntax 
+            //var query2 = manufacturers.GroupJoin(cars,
+            //                                     m => m.Name,
+            //                                     c => c.Manufacturer,
+            //                                     (m, g) => new
+            //                                     {
+            //                                         manufacturer = m,
+            //                                         cars = g
+            //                                     }).OrderBy(m => m.manufacturer.Name);
+
+            //Console.WriteLine();
+            //Console.WriteLine("***** Grouping Data with Method Syntax *****");
+            //Console.WriteLine();
+            //foreach (var group in query2)
+            //{
+            //    Console.WriteLine($"{group.manufacturer.Name} : {group.manufacturer.Headquarters}");
+            //    foreach (var car in group.cars.OrderByDescending(c => c.Combined).Take(2))
+            //    {
+            //        Console.WriteLine($"\t{car.Name} : {car.Combined}");
+            //    }
+
+            //}
+
+            #endregion
+
+
+            #region Aggregating Data with Query Syntax 
             var query =
-                from manufacturer in manufacturers
-                join car in cars
-                on manufacturer.Name equals car.Manufacturer
-                into carGroup
-                orderby manufacturer.Name
+                from car in cars
+                group car by car.Manufacturer into carGroup
                 select new
                 {
-                    manufacturer = manufacturer,
-                    cars = carGroup
-                };
+                    Name = carGroup.Key,
+                    Max = carGroup.Max(c => c.Combined),
+                    Min = carGroup.Min(c => c.Combined),
+                    Avg = carGroup.Average(c => c.Combined),
+
+                } into result
+                orderby result.Max descending
+                select result;
+
 
 
 
             Console.WriteLine();
-            Console.WriteLine("***** Grouping Data with Query Syntax  *****");
+            Console.WriteLine("***** Aggregating Data with Query Syntax   *****");
             Console.WriteLine();
-            foreach (var group in query)
+            foreach (var result in query)
             {
-                Console.WriteLine($"{group.manufacturer.Name} : {group.manufacturer.Headquarters}");
-                foreach (var car in group.cars.OrderByDescending(c => c.Combined).Take(2))
-                {
-                    Console.WriteLine($"\t{car.Name} : {car.Combined}");
-                }
+                Console.WriteLine($"{result.Name}");
+                Console.WriteLine($"\t Max : {result.Max}");
+                Console.WriteLine($"\t Min : {result.Min}");
+                Console.WriteLine($"\t Avg : {result.Avg}");
+
 
             }
             #endregion
+            #region Aggregating Data with Method Syntax 
+            var query2 =
+               cars.GroupBy(c => c.Manufacturer)
+                   .Select(g =>
+                   {
+                       var results = g.Aggregate(new CarStatistics(),
+                                           (acc, c) => acc.Accumulate(c),
+                                           acc => acc.Compute());
+                       return new
+                       {
+                           Name = g.Key,
+                           Avg = results.Average,
+                           Min = results.Min,
+                           Max = results.Max
+                       };
+                   })
+                   .OrderByDescending(r => r.Max);
 
-            #region Using a GroupJoin for Hierarchical Data with Method Syntax 
-            var query2 = manufacturers.GroupJoin(cars,
-                                                 m => m.Name,
-                                                 c => c.Manufacturer,
-                                                 (m, g) => new
-                                                 {
-                                                     manufacturer = m,
-                                                     cars = g
-                                                 }).OrderBy(m => m.manufacturer.Name);
-
-            Console.WriteLine();
-            Console.WriteLine("***** Grouping Data with Method Syntax *****");
-            Console.WriteLine();
-            foreach (var group in query2)
+            foreach (var result in query2)
             {
-                Console.WriteLine($"{group.manufacturer.Name} : {group.manufacturer.Headquarters}");
-                foreach (var car in group.cars.OrderByDescending(c => c.Combined).Take(2))
-                {
-                    Console.WriteLine($"\t{car.Name} : {car.Combined}");
-                }
-
+                Console.WriteLine($"{result.Name}");
+                Console.WriteLine($"\t Max: {result.Max}");
+                Console.WriteLine($"\t Min: {result.Min}");
+                Console.WriteLine($"\t Avg: {result.Avg}");
             }
-
             #endregion
         }
 
@@ -183,6 +240,36 @@ namespace Cars
                        });
             return query.ToList();
         }
+
+    }
+    public class CarStatistics
+    {
+        public CarStatistics()
+        {
+            Max = Int32.MinValue;
+            Min = Int32.MaxValue;
+        }
+
+        public CarStatistics Accumulate(Car car)
+        {
+            Count += 1;
+            Total += car.Combined;
+            Max = Math.Max(Max, car.Combined);
+            Min = Math.Min(Min, car.Combined);
+            return this;
+        }
+
+        public CarStatistics Compute()
+        {
+            Average = Total / Count;
+            return this;
+        }
+
+        public int Max { get; set; }
+        public int Min { get; set; }
+        public int Total { get; set; }
+        public int Count { get; set; }
+        public double Average { get; set; }
 
     }
 
